f18b98f16a858574af23a5984ff3776be86a1c3c
Valkyrie System <36176120+valkyrie-system@users.noreply.github.com>
2025-06-04 19:58:45 -0500

    Add files via upload

---
+++ autostart-scripts/play-boot-sound.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Wait for splash to finish
+sleep 5.1  # 5.06 rounded up slightly for reliability
+
+# Play your boot sound
+paplay "/home/valkyrie-sys/.config/autostart-scripts/Nintendo_Gamecube_Boot.wav
