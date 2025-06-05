1c20af6aff38b05e95801a46e245a11081ffa0d1
Valkyrie System <36176120+valkyrie-system@users.noreply.github.com>
2025-06-05 11:28:36 -0500

    Delete autostart-scripts directory

--- autostart-scripts/play-boot-sound.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Wait for splash to finish
-sleep 5.1  # 5.06 rounded up slightly for reliability
-
-# Play your boot sound
-paplay "/home/PutYourUserHere/.config/autostart-scripts/Nintendo_Gamecube_Boot.wav
